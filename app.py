95678d22f2505f241ae9395ae1964634a60bdb30
Zaid <zaid.k@peerbits.com>
2025-12-26 12:16:14 +0530

    Added get_text_from_file function in utils.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 from routes import register_routes
+from flask_cors import CORS
 
 from dotenv import load_dotenv
 load_dotenv()
 
 
 app = Flask(__name__)
-
+CORS(app)
 api = Api(app)
 
 from config.pinecone_service.pc_config import ensure_pinecone_index
@@ -16,5 +17,5 @@ ensure_pinecone_index()
 
 
 if __name__ == '__main__':
-    register_routes(api=api)
-    app.run(port=9833, debug=True, host='0.0.0.0')
+    register_routes(api = api)
+    app.run(port = 9833, debug = True, host = '0.0.0.0')
--- config/utils.py
+++ config/utils.py
@@ -1,4 +1,7 @@
 from config.pinecone_service.pc_config import generate_embedding, get_similar_cases
+from docx import Document
+from pypdf import PdfReader
+import io
 
 
 def get_top_cases(jd_summary, res_summary, user_id):
@@ -10,3 +13,20 @@ def get_top_cases(jd_summary, res_summary, user_id):
     print("top cases list-->", top_cases_list)
 
     return top_cases_list
+
+
+def get_text_from_file(file):
+    file_name = file.filename.lower()
+
+    if file_name.endswith(".pdf"):
+        reader = PdfReader(io.BytesIO(file.read()))
+        return "".join(p.extract_text() for p in reader.pages)
+
+    if file_name.endswith(".docx"):
+        doc = Document(io.BytesIO(file.read()))
+        return "\n".join(p.text for p in doc.paragraphs)
+
+    if file_name.endswith(".txt"):
+        return file.read().decode("utf-8")
+
+    return None
